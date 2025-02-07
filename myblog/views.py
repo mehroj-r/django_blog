99731ddb48547607c90959e440ca27cdd3203407
Mehroj <mekhrojmajidov@gmail.com>
2025-02-07 10:32:39 +0500

    Added templates, statics & Frontend

--- myblog/views.py
+++ myblog/views.py
@@ -2,4 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def post(request):
+    return render(request, 'post.html')
