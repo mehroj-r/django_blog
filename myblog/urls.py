66452e9acdf792b665f4db340aeef6f82bf9fa83
Mehroj <mekhrojmajidov@gmail.com>
2025-02-07 11:27:25 +0500

    Small fixes

--- myblog/urls.py
+++ myblog/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about', views.about, name='about'),
     path('contact', views.contact, name='contact'),
-    path('post', views.post, name='post'),
     path('post/<int:id>', views.postID, name='post'),
 
 ]
--- myblog/views.py
+++ myblog/views.py
@@ -1,14 +1,10 @@
 from django.shortcuts import render
-
 from myblog.models import Post
 
 
 def index(request):
 
     posts = Post.objects.all()
-    for post in posts:
-        print(post.title)
-
     return render(request, 'index.html', {'posts':posts})
 
 def about(request):
@@ -19,7 +15,6 @@ def contact(request):
 
 def post(request):
     return render(request, 'post.html')
-
 
 def postID(request, id):
     postByID = Post.objects.get(id=id)
